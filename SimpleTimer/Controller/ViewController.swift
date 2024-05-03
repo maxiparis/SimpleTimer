6529fd61ebcdbed523c4824fc9140bd02a4969b2
“Max <mparis@hotmail.cl>
2024-05-03 10:29:15 -0600

    implemented a time picker

--- SimpleTimer/Controller/ViewController.swift
+++ SimpleTimer/Controller/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import RealTimePicker
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var subView: UIView!
+    private var timePicker: RealTimePickerView = {
+        let view = RealTimePickerView(format: .h24)
+        view.showUnitSeparator = true
+        view.rowHeight = 40 // default row height is 60
+        view.timeLabelFont = UIFont.systemFont(ofSize: 32, weight: .semibold) // default size
+        view.colonLabelFont = UIFont.systemFont(ofSize: 32 * 0.75, weight: .bold) // default size
+        view.formatLabelFont = UIFont.systemFont(ofSize: 20, weight: .semibold) // default size
+        view.backgroundColor = UIColor.white.withAlphaComponent(0.9)
+        view.layer.cornerRadius = 24
+        view.showCurrentTime = true
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
     }
-
+    
+    private func setupViews() {
+//        subView.addSubview(timePicker)
+        view.addSubview(timePicker)
+        timePicker.translatesAutoresizingMaskIntoConstraints = false
+        timePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        timePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        timePicker.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
+    }
 
 }
 
